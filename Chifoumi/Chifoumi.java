928335cbd445496ab0d87890ce44b3928b1c6b61
ICEBergJQ <salahsk52@gmail.com>
2025-09-22 13:54:38 +0100

    adding wara9a 7ajara mi9ass hh

---
+++ Chifoumi/Chifoumi.java
@@ -0,0 +1,13 @@
+public class Chifoumi {
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+        switch (chifoumiAction) {
+            case ROCK:
+                return ChifoumiAction.PAPER;
+            case PAPER:
+                return ChifoumiAction.SCISSOR;
+            case SCISSOR:
+                return ChifoumiAction.ROCK;
+        }
+        return chifoumiAction.PAPER;
+    }
+}
