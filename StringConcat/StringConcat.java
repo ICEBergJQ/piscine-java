6d0d829962b8e915d14204d9cc112f2cf6cb249b
ICEBergJQ <salahsk52@gmail.com>
2025-09-22 14:08:10 +0100

    fixed

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,8 +1,8 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
-        if (s1 == null) s1 = "";
-        if (s2 == null) s2 = "";
         if (s2 == null && s1 == null) return null;
+        if (s1 == null) return s2;
+        if (s2 == null) return s1;
         return s1 + s2;
     }
 }
