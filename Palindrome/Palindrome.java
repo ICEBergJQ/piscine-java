02a6fb7fcae2f4ca9da5f926f2450a8498891c87
ICEBergJQ <salahsk52@gmail.com>
2025-09-23 14:44:54 +0100

    adding palindrome

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
