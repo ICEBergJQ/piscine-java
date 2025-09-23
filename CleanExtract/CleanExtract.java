7099a1d5e646a6b437313a1872738696fa648cf0
ICEBergJQ <salahsk52@gmail.com>
2025-09-23 14:26:59 +0100

    fixed

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -3,11 +3,22 @@ public class CleanExtract {
         String[] parts = s.split("\\|");
         String result = "";
 
-        for (String part : parts) {
-            part = part.trim();
+        for (int y = 0; y < parts.length; y++) {
+            String part = parts[y].trim();
             Boolean flag = part.endsWith(".");
-            part = part.replace(".", "");
-             part = part.trim();
+            String temp = "";
+            Boolean f = false;
+            for (int i = 0; i < part.length(); i++) {
+                 if (part.charAt(i) == '.') {
+                     f = true;
+                 }
+                 if (f) {
+                    temp += part.charAt(i);
+                 }
+
+            }
+            part = temp.replace(".", "");
+            part = part.trim();
             if (flag) {
                 part = part+".";
             }
@@ -19,11 +30,17 @@ public class CleanExtract {
                 result += part;
             }
         }
+        // System.out.println("\'"+result+"\'");
+
         if (result.endsWith(".")) {
+            result = result.replace(" .", "");
             result = result.replace(".", "");
             return result + ".";
         }
 
-        return result;
+        return result.replace(".", "");
     }
 }
+
+// 'The quick brown fox Jumps over the lazy dog.', 'The quick brown what do you fox Jumps over the lazy dog.' was returned
+// java.lang.AssertionError: Should return 'The quick brown fox Jumps over the lazy dog.', 'The quick brown what do you fox Jumps over the lazy dog.'
