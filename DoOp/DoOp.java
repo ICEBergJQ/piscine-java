08d26be5895f67c2274cef3f6963a8966ce02752
ICEBergJQ <salahsk52@gmail.com>
2025-09-23 14:54:29 +0100

    adding doop

---
+++ DoOp/DoOp.java
@@ -0,0 +1,43 @@
+package DoOp;
+public class DoOp {
+    public static String operate(String[] args) {
+        if (args.length < 3) {
+            return "Error";
+        }
+
+        int left, right;
+        try {
+            left = Integer.parseInt(args[0]);
+            right = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            return "Error";
+        }
+
+        String op = args[1];
+        int result;
+
+        switch (op) {
+            case "+":
+                result = left + right;
+                break;
+            case "-":
+                result = left - right;
+                break;
+            case "*":
+                result = left * right;
+                break;
+            case "/":
+                if (right == 0) return "Error";
+                result = left / right;
+                break;
+            case "%":
+                if (right == 0) return "Error";
+                result = left % right;
+                break;
+            default:
+                return "Error";
+        }
+
+        return String.valueOf(result);
+    }
+}
