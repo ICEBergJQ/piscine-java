6372bd22264cfe826de2e031143c1e9cdcf328a6
ICEBergJQ <salahsk52@gmail.com>
2025-09-24 14:04:11 +0100

    adding cat

---
+++ Cat/Cat.java
@@ -0,0 +1,27 @@
+package Cat;
+
+// import java.util.Scanner;
+import java.io.*;
+
+public class Cat {
+    public static void cat(String[] args) throws IOException {
+
+        if (args.length == 0) {
+            return;
+        }
+        for (String arg : args) {
+            try (FileInputStream fis = new FileInputStream(arg)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                while ((bytesRead = fis.read(buffer)) != -1) {
+                    System.out.write(buffer, 0, bytesRead);
+                }
+
+                System.out.flush();
+            } catch (FileNotFoundException e) {
+                return;
+            }
+        }
+    }
+}
