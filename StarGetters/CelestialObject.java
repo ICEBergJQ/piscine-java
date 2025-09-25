90696e5248523719de0c31e228fe0d5032f64a5a
ICEBergJQ <salahsk52@gmail.com>
2025-09-25 14:13:57 +0100

    adding stargetters

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,51 @@
+package StarGetters;
+
+public class CelestialObject {
+    // private properties
+    private double x;
+    private double y;
+    private double z;
+    private String name;
+
+    // default constructor
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    // getters
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // setters
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
