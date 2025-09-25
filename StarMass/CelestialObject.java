e1d33fd9b5f95f133a0a880181da885c28f8c5a2
ICEBergJQ <salahsk52@gmail.com>
2025-09-25 17:36:31 +0100

    fixed

--- StarMass/CelestialObject.java
+++ StarMass/CelestialObject.java
@@ -19,22 +19,52 @@ public class CelestialObject {
         this.mass = mass;
     }
 
-    public String getName() { return name; }
-    public double getX() { return x; }
-    public double getY() { return y; }
-    public double getZ() { return z; }
-    public int getMass() { return mass; }
+    public String getName() {
+        return name;
+    }
 
-    public void setName(String name) { this.name = name; }
-    public void setX(double x) { this.x = x; }
-    public void setY(double y) { this.y = y; }
-    public void setZ(double z) { this.z = z; }
-    public void setMass(int mass) { this.mass = mass; }
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof CelestialObject)) return false;
+        if (this == o)
+            return true;
+        if (!(o instanceof CelestialObject))
+            return false;
         CelestialObject that = (CelestialObject) o;
         return Double.compare(that.x, x) == 0 &&
                 Double.compare(that.y, y) == 0 &&
@@ -53,10 +83,16 @@ public class CelestialObject {
         return String.format("%s is positioned at (%.3f, %.3f, %.3f)", name, x, y, z);
     }
 
+    public static final double KM_IN_ONE_AU = 150_000_000;
+
     public static double getDistanceBetween(CelestialObject a, CelestialObject b) {
         double dx = a.x - b.x;
         double dy = a.y - b.y;
         double dz = a.z - b.z;
         return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject a, CelestialObject b) {
+        return getDistanceBetween(a, b) * KM_IN_ONE_AU;
     }
 }
