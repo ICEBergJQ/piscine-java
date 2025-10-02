761a5f0ad64e84988c31f82f4853171ae7f86607
ICEBergJQ <salahsk52@gmail.com>
2025-10-02 13:10:31 +0100

    adding listSearchindex

---
+++ ListSearchIndex/ListSearchIndex.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class ListSearchIndex {
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i).equals(value)) {
+                return i;
+            }
+        }
+        return null;
+    }
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(value)) {
+                return i;
+            }
+        }
+        return null;
+    }
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        List<Integer> indexes = new java.util.ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(value)) {
+                indexes.add(i);
+            }
+        }
+        return indexes;
+    }
+}
