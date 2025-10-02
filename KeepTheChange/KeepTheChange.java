ff3cf13fe1058c923f4655ce20dcc7c9a9ee17b8
ICEBergJQ <salahsk52@gmail.com>
2025-10-02 16:42:50 +0100

    adding keep the change

---
+++ KeepTheChange/KeepTheChange.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class KeepTheChange {
+
+    public static List<Integer> computeChange(int amount, Set<Integer> coins) {
+        List<Integer> result = new ArrayList<>();
+
+        List<Integer> sortedCoins = new ArrayList<>(coins);
+        Collections.sort(sortedCoins, Collections.reverseOrder());
+
+        for (int coin : sortedCoins) {
+            while (amount >= coin) {
+                amount -= coin;
+                result.add(coin);
+            }
+        }
+
+        return result;
+    }
+}
