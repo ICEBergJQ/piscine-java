5c5972e42f67aff28e39780fa080d271f11eba9a
ICEBergJQ <salahsk52@gmail.com>
2025-10-07 13:02:25 +0100

    adding regexmatch

---
+++ RegexMatch/RegexMatch.java
@@ -0,0 +1,14 @@
+public class RegexMatch {
+    public static boolean containsOnlyAlpha(String s) {
+        return s.matches("^[A-Za-z]+$");
+    }
+
+    public static boolean startWithLetterAndEndWithNumber(String s) {
+        return s.matches("^[A-Za-z].*[0-9]$");
+    }
+
+    public static boolean containsAtLeast3SuccessiveA(String s) {
+        return s.matches(".*A{3,}.*");
+    }
+
+}
