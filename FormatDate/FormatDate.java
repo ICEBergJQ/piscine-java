3af13c33a02fa19da7590ff3a400c6502b30ece4
ICEBergJQ <salahsk52@gmail.com>
2025-10-07 12:59:05 +0100

    fixed ig

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -7,9 +7,10 @@ import java.util.Locale;
 public class FormatDate {
 
     public static String formatToFullText(LocalDateTime dateTime) {
-        if (dateTime == null) return null;
+        if (dateTime == null)
+            return null;
 
-        DateTimeFormatter dayMonthFormatter = DateTimeFormatter.ofPattern("d MMMM", Locale.FRENCH);
+        DateTimeFormatter dayMonthFormatter = DateTimeFormatter.ofPattern("d MMM", Locale.FRENCH);
         String formattedDate = dateTime.format(dayMonthFormatter);
         int year = dateTime.getYear();
         int hour = dateTime.getHour();
@@ -20,14 +21,16 @@ public class FormatDate {
     }
 
     public static String formatSimple(LocalDate date) {
-        if (date == null) return null;
+        if (date == null)
+            return null;
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN);
         return date.format(formatter);
     }
 
     public static String formatIso(LocalTime time) {
-        if (time == null) return null;
+        if (time == null)
+            return null;
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         return time.format(formatter);
