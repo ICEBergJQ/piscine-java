4bef109dc400bed499d71b3b029b8e792aab4960
ICEBergJQ <salahsk52@gmail.com>
2025-10-07 12:54:48 +0100

    adding formatdate

---
+++ FormatDate/FormatDate.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class FormatDate {
+
+    public static String formatToFullText(LocalDateTime dateTime) {
+        if (dateTime == null) return null;
+
+        DateTimeFormatter dayMonthFormatter = DateTimeFormatter.ofPattern("d MMMM", Locale.FRENCH);
+        String formattedDate = dateTime.format(dayMonthFormatter);
+        int year = dateTime.getYear();
+        int hour = dateTime.getHour();
+        int minute = dateTime.getMinute();
+        int second = dateTime.getSecond();
+
+        return String.format("Le %s de l'an %d à %dh%dm et %ds", formattedDate, year, hour, minute, second);
+    }
+
+    public static String formatSimple(LocalDate date) {
+        if (date == null) return null;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN);
+        return date.format(formatter);
+    }
+
+    public static String formatIso(LocalTime time) {
+        if (time == null) return null;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.n");
+        return time.format(formatter);
+    }
+}
