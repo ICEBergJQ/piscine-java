1a814f3ddb29393c80c20f5451d509406eb3cfc8
ICEBergJQ <salahsk52@gmail.com>
2025-10-07 12:45:15 +0100

    adding parsedata

---
+++ ParseDate/ParseDate.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class ParseDate {
+
+    public static LocalDateTime parseIsoFormat(String stringDate) {
+        return LocalDateTime.parse(stringDate);
+    }
+
+    public static LocalDate parseFullTextFormat(String stringDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy", Locale.ENGLISH);
+        return LocalDate.parse(stringDate, formatter);
+    }
+
+    public static LocalTime parseTimeFormat(String stringDate) {
+        String lower = stringDate.toLowerCase();
+
+        int hours = Integer.parseInt(lower.split(" hours")[0].trim());
+        int minutes = Integer.parseInt(lower.split(" ")[6].trim());
+        int seconds = Integer.parseInt(lower.split(" ")[10].trim());
+
+        if (lower.contains("evening") && hours < 12) {
+            hours += 12;
+        }
+
+        return LocalTime.of(hours, minutes, seconds);
+    }
+}
